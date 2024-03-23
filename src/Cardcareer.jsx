b5d4ef5355f156bfc17001252c3ffe4a773c37b0
Johnvilin-Sibina <johnvilinsibina@gmail.com>
2024-03-23 10:29:44 +0530

    Width of the card is adjusted.

--- src/Cardcareer.jsx
+++ src/Cardcareer.jsx
@@ -1,7 +1,7 @@
 function Cardcareer({article}) {
   return (
     <div className="col-md-4 mt-5">
-      <div className="card" style={{ width: "18rem", height: "350px"}}>
+      <div className="card" style={{ width: "16rem", height: "350px"}}>
         <img src={article.image} style={{height: "150px"}} className="card-img-top" alt="..." />
         <div className="card-body">
           <p className="card-text">
